32664c0bd6994f51860350838f5e9bf3d821633e
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-07-22 19:07:40 +0900

    Fix : Security Setting

--- src/main/java/com/jmt/v1/config/SecurityConfig.java
+++ src/main/java/com/jmt/v1/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -13,8 +14,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests(request->
-                        request.antMatchers(HttpMethod.OPTIONS,"").permitAll()
+                        request.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                 )
         ;
     }
